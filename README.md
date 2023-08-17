c36532b6c533b392f7cee3037693494b5854fa8f
okabe <923048992@qq.com>
2023-08-17 10:53:40 +0800

    initial commit

---
+++ README.md
@@ -0,0 +1,6 @@
+# apod
+
+This repo will crawl apod(Astronomy Picture of the Day) from https://apod.nasa.gov/apod/ every day, using **Github Actions**.
+
+<div>
+</div>
---
+++ main.py
@@ -0,0 +1,63 @@
+import datetime
+import os
+
+import regex
+import requests
+from bs4 import BeautifulSoup
+from markdown import markdownFromFile
+
+if __name__ == '__main__':
+    url = 'https://apod.nasa.gov/apod/'
+    try:
+        r = requests.get(url)
+        doc = BeautifulSoup(r.content, parser='html.parser', features="lxml")
+        img_ele = doc.find('img')
+        img_ref = f"https://apod.nasa.gov/apod/{img_ele.parent.get('href')}"
+        explanation_fst_ele = doc.find('b', text=regex.compile('.*Explanation.*'))
+        explanation_para_ele = explanation_fst_ele.parent
+        explanation = str(explanation_para_ele).replace('\n', '')
+
+        markdownFromFile(input='./README.md', output='./tmp.html', output_format='html')
+        today_time = datetime.datetime.now().strftime('%Y/%m/%d')
+
+        with open('./tmp.html', 'r') as f:
+            doc = BeautifulSoup(f.read(), parser='html.parser', features="lxml")
+            details_parent = doc.find('div')
+            details = doc.findAll('details')
+            if len(details) > 0:
+                yesterday = datetime.datetime.now() - datetime.timedelta(days=1)
+                yesterday_url = yesterday.strftime("https://apod.nasa.gov/apod/ap%y%m%d.html")
+                original = details[0].findAll('td')[1]
+                new_original = BeautifulSoup(f'<td>Original url: <a>{yesterday_url}</a></td>', features="lxml").find(
+                    'td')
+                original.replace_with(new_original)
+
+            today_apod = f'<details>\n' \
+                         f'<summary>{today_time}</summary>\n' \
+                         f'<table>\n' \
+                         f'<tr>\n' \
+                         f'<td><img src="{img_ref}" alt=""/></td>\n' \
+                         f'</tr>\n' \
+                         f'<tr>\n' \
+                         f'<td>Original url: <a>{url}</a></td>\n' \
+                         f'</tr>\n' \
+                         f'<tr>\n' \
+                         f'<td>\n' \
+                         f'{explanation}\n' \
+                         f'</td>\n' \
+                         f'</tr>\n' \
+                         f'</table>\n' \
+                         f'</details>\n'
+
+            details_parent.insert(0, BeautifulSoup(today_apod, features='lxml').find('details'))
+            with open('./README.md', 'w') as f:
+                content = f'# apod\n\n' \
+                          f'This repo will crawl apod(Astronomy Picture of the Day) from https://apod.nasa.gov/apod/ ' \
+                          f'every day, using **Github Actions**.\n\n' \
+                          f'{details_parent.prettify()}'
+                f.write(content)
+
+        os.remove('./tmp.html')
+    except Exception as e:
+        print(e)
+        exit(-1)
